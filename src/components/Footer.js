ac477bef5535549d654f24646d2e2177f0a2be0d
Domingo Rivera <djr@infosecusa.com>
2019-03-08 02:09:03 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,7 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                   <li>
-                    <Link className="navbar-item" to="/blog">
+                    <Link className="navbar-item" to="/laws">
                       Latest Stories
                     </Link>
                   </li>
