c1b1c8f1abc09ab0c3cab8001f05a231f4f90fa1
Domingo Rivera <djr@infosecusa.com>
2019-03-08 02:04:37 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -97,7 +97,7 @@ export const IndexPageTemplate = ({
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                  <Link className="btn" to="/blog">
+                  <Link className="btn" to="/laws">
                     Read more
                   </Link>
                   </div>
