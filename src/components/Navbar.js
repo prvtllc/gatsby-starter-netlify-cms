7980f32aa7f52cf6a5f2c0b3e27f6678a0f82c80
Domingo Rivera <djr@infosecusa.com>
2019-03-11 01:12:38 -0400

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo-1.png'
+import logo from '../img/logo.png'
 
 const Navbar = class extends React.Component {
   componentDidMount() {
