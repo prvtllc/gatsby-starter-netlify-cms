8694704ac06daa7e2344197d60ecd3e3fc2b3899
Domingo Rivera <djr@infosecusa.com>
2019-03-12 22:21:28 -0400

    Revert "Test"
    This reverts commit 5136425f976240a4b7ec5d2aef71acbb88d8708c.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,10 @@ import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('resources', ProductPagePreview)
 CMS.registerPreviewTemplate('laws', BlogPostPreview)
